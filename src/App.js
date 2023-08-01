e8c89e4e62cf47ee3fd4c6b58cd8462d180a8fab
iramnaaz <iramnaazkhan314@gmail.com>
2023-08-01 11:54:29 +0530

    new modified files

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Clock from './Clock';
 import CarList from './Carlist';
 import CarSearch from './CarSearch';
 
-const carData = [
-  { make: 'Honda', model: 'Vezel', condition: 'New', price: 2000 },
-  { make: 'Honda', model: 'Civic', condition: 'Used', price: 3000 },
- 
-];
+
 
 const App = () => {
-  const [filteredCars, setFilteredCars] = useState(carData);
-
-  const handleSearch = (term) => {
-    const filtered = carData.filter(
-      (car) =>
-        car.make.toLowerCase().includes(term.toLowerCase()) ||
-        car.model.toLowerCase().includes(term.toLowerCase())
-    );
-    setFilteredCars(filtered);
-  };
+ 
 
   return (
     <div>
       <h5>Current Time: <Clock /></h5>
      
-      <CarList cars={filteredCars} />
-      <h2>CarSearch</h2>
-      <CarSearch placeholder='Enter a car detail'  data={handleSearch}/>
+      <CarList/>
+      <CarSearch/>
     </div>
   );
 };
--- src/CarSearch.jsx
+++ src/CarSearch.jsx
@@ -1,78 +1,68 @@
 import React, { useState } from 'react';
-import Table from 'react-bootstrap/Table';
 
 const CAR_LIST = [
-           {
-            id: 1,
-            make: "Mitsubishi",
-            model: "Lancer",
-            type: "Used"
-          },
-          { id: 2, make: "Honda", model: "Vezel", type: "New" },
-          { id: 3, make: "Honda", model: "Civic", type: "Used" },
-          { id: 4, make: "Audi", model: "A3", type: "New" },
-          { id: 5, make: "Audi", model: "A4", type: "Used" },
-          { id: 6, make: "Audi", model: "A7", type: "New" },
-          { id: 7, make: "BMW", model: "i8", type: "Used" }
-         ]
-
-const PRICE_LIST = [
-    { car_id: 1, price: 1000000 },
-    { car_id: 2, price: 2000000 },
-    { car_id: 3, price: 3000000 },
-    { car_id: 4, price: 4000000 },
-    { car_id: 5, price: 5000000 },
-    { car_id: 6, price: 6000000 },
-    { car_id: 7, price: 7000000 }
+  { id: 1, make: "Mitsubishi", model: "Lancer", type: "Used" },
+  { id: 2, make: "Honda", model: "Vezel", type: "New" },
+  { id: 3, make: "Honda", model: "Civic", type: "Used" },
+  { id: 4, make: "Audi", model: "A3", type: "New" },
+  { id: 5, make: "Audi", model: "A4", type: "Used" },
+  { id: 6, make: "Audi", model: "A7", type: "New" },
+  { id: 7, make: "BMW", model: "i8", type: "Used" }
 ];
 
-const CarTable = () => {
-  const [searchQuery, setSearchQuery] = useState('');
+const SearchBox = () => {
+  const [searchTerm, setSearchTerm] = useState({ make: '', model: '' });
+  const [searchResults, setSearchResults] = useState([]);
 
-  const handleSearchChange = (event) => {
-    setSearchQuery(event.target.value);
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setSearchTerm((prevState) => ({ ...prevState, [name]: value }));
   };
 
-  const filteredCars = CAR_LIST.filter((car) =>
-    car.model.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  const handleSearch = () => {
+    const filteredCars = CAR_LIST.filter(
+      (car) =>
+        car.make.toLowerCase().includes(searchTerm.make.toLowerCase()) &&
+        car.model.toLowerCase().includes(searchTerm.model.toLowerCase())
+    );
+    setSearchResults(filteredCars);
+  };
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Search car model..."
-        value={searchQuery}
-        onChange={handleSearchChange}
-        style={{ width: '100%', padding: '8px', marginBottom: '16px' }}
-      />
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th style={{border:' 1px solid'}}>ID</th>
-            <th style={{border:' 1px solid'}}>Make</th>
-            <th style={{border:' 1px solid'}}>Model</th>
-            <th style={{border:' 1px solid'}}>Type</th>
-            <th style={{border:' 1px solid'}}>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredCars.map((car) => {
-            const carPrice = PRICE_LIST.find((price) => price.car_id === car.id);
-            return (
-              <tr key={car.id}>
-                <td style={{border:' 1px solid'}}>{car.id}</td>
-                <td style={{border:' 1px solid'}}>{car.make}</td>
-                <td style={{border:' 1px solid'}}>{car.model}</td>
-                <td style={{border:' 1px solid'}}>{car.type}</td>
-                <td style={{border:' 1px solid'}}>{carPrice ? carPrice.price : 'N/A'}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
+      <h1>Car Search</h1>
+      <div>
+        <input
+          type="text"
+          name="make"
+          value={searchTerm.make}
+          placeholder="Enter make..."
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="model"
+          value={searchTerm.model}
+          placeholder="Enter model..."
+          onChange={handleInputChange}
+        />
+        
+        <button onClick={handleSearch}>Search</button>
+      </div>
+
+      <div>
+        <h2>Search Results:</h2>
+        <ul>
+          {searchResults.map((car) => (
+            <li key={car.id}>
+              {car.make} {car.model} - {car.type}
+            </li>
+          ))}
+        </ul>
+      </div>
+      
     </div>
   );
 };
 
-export default CarTable;
+export default SearchBox;
